36ec69d68301be160f2ef6fcc397e478b81ab73c
William Dixon <imphocused@gmail.com>
2021-02-06 18:40:24 -0600

    new email

--- contact.php
+++ contact.php
@@ -3,7 +3,7 @@
 	/* ==========================  Define variables ========================== */
 
 	#Your e-mail address
-	define("__TO__", "besimdauti24@gmail.com");
+	define("__TO__", "janice@notaryj.com");
 
 	#Message subject
 	define("__SUBJECT__", "examples.com = From:");
